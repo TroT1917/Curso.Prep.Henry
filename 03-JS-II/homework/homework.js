157ab70ee802e75703c0c8c5dc9d1b3187ddb405
TroT1917 <leontuerca@hotmail.com>
2020-06-21 03:11:05 -0300

    asdads

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -83,10 +83,10 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
   if (numero === 0 || numero === 1) return false;
   if (numero === 2)return false;
-  for (i=2;i < numero;i++){
-    if (numero % i === 0)return true
+  for (var i=2;i < numero;i++){
+    if (numero % i === 0)return false
   } 
-
+  return true;
 }
 
 
